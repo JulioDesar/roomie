01d2d7b8612afe7a0243629814d661d5d08eeb58
JulioDesar <cesarj598@gmail.com>
2022-09-11 19:36:38 -0300

    first commit

---
+++ src/components/Login-form/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export function LoginForm() {
+    return (
+        <header>
+            <img width={385} src="./logo.svg" alt="Roomie" />
+        </header>
+    );
+}
+
