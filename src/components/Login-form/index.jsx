c748bb5bef549a352ac45886a676717ce2ee8b75
VitorDallaa <vitordallatorre@gmail.com>
2022-09-12 20:10:28 -0300

    login page almost done

--- src/components/Login-form/index.jsx
+++ src/components/Login-form/index.jsx
@@ -5,14 +5,15 @@ import "./style.css";
 export function LoginForm() {
     return (
         <header>
-            <img width={385} src="./logo.svg" alt="Roomie" />
+            <img width={400} src="./logo.svg" alt="Roomie" />
 
-            <span>E-mail:</span>
-            <Input type="email" size="50" />
+            <span>Login</span>
+            <Input type="email" size="75" />
 
-            <span>Senha:</span>
-            <Input type="password" size="50" />
-            <button type="submit" value="Submit">Enviar</button>
+            <span>Senha</span>
+            <Input type="password" size="75" />
+            <a href="*" id="forgot-password">Esqueci minha senha</a>
+            <button type="submit" value="Submit" className="Login-button">Entrar</button>
         </header>
     );
 }
