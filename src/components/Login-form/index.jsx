9c7568ae1835fc189c0b21602a748dfc9dc2f3f8
VitorDallaa <vitordallatorre@gmail.com>
2022-09-12 21:05:21 -0300

    changes login page

--- src/components/Login-form/index.jsx
+++ src/components/Login-form/index.jsx
@@ -7,13 +7,20 @@ export function LoginForm() {
         <header>
             <img width={400} src="./logo.svg" alt="Roomie" />
 
-            <span>Login</span>
-            <Input type="email" size="75" />
+            <form action="">
+                <span>Login</span>
+                <Input type="email" size="75" />
 
-            <span>Senha</span>
-            <Input type="password" size="75" />
-            <a href="*" id="forgot-password">Esqueci minha senha</a>
-            <button type="submit" value="Submit" className="Login-button">Entrar</button>
+                <span>Senha</span>
+                <Input type="password" size="75" />
+                <a href="*" id="forgot-password">Esqueci minha senha</a>
+                <button type="submit" value="Submit" className="Login-button">Entrar</button>
+            </form>
+
+            <section>
+                <img src="./google_svg.svg" alt="" />
+                <img src="./instagram_svg.svg" alt="" />
+            </section>
         </header>
     );
 }
