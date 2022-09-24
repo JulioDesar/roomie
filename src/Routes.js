890c2f5ffee50ba32136f421b4a263547eb1f045
VitorDallaa <vitordallatorre@gmail.com>
2022-09-24 16:41:07 -0300

    Atualizando Arquivos
    Atualizei todo o projeto para poder incluir as modificações que eu fiz.

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,36 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+/*import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 
+/*<Route path="/" element={<Login/>} />
+*/
+import React from "react";
+import Admin from "./pages/AdminCliente";
+import Login from "./pages/LoginCliente";
+import Teste from "./pages/TesteForm";
+/*import Login from "./pages/LoginCliente";*/
+
+
+
 function RoutesApp() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/Admin" element={<Admin/>} />
       </Routes>
+
+      <Routes>
+        <Route path="/Login" element={<Login/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/Teste" element={<Teste/>} />
+      </Routes>
+
     </BrowserRouter>
+    
   );
 }
 
--- src/pages/LoginCliente/index.js
+++ src/pages/LoginCliente/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import "./style.css";
+
 import { LoginForm } from "../../components/Login-form";
 import "./style.css"
 
